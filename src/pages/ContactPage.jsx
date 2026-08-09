e9292eb37513b98cf06cbe84a25c6e6dc73f950a
busecllskn <buseclskn738@gmail.com>
2026-08-10 02:24:57 +0300

    add: contact page

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactSection from '../components/ContactSection';
+
+const ContactPage = () => {
+  return (
+    <div className="w-full min-h-screen bg-[#252B42]">
+      <ContactSection />
+    </div>
+  );
+};
+
+export default ContactPage;
