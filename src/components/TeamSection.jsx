ba9d5b2722a10d22bd10af5a68ec7edb65a86f51
busecllskn <buseclskn738@gmail.com>
2026-08-13 00:33:57 +0300

    T06

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,45 +1,54 @@
 import React from 'react';
 
+import team1 from '../assets/team.jpg';
+import team22 from '../assets/team22.jpg';
+import team33 from '../assets/team33.png';
+import team4 from '../assets/team4.jpg';
+import team5 from '../assets/team5.jpg';
+import team6 from '../assets/team6.jpg';
+import team7 from '../assets/team7.jpg';
+import team8 from '../assets/team8.jpg';
+
 const teamMembers = [
   {
     name: 'Gökhan Özdemir',
     role: 'Project Manager',
-    image: '../assets/team.jpg',
+    image: team1,
   },
   {
     name: 'Buse',
     role: 'Full Stack Developer',
-    image: '../assets/team2.jpg',
+    image: team22,
   },
   {
-    name: 'Ronald Richards',
-    role: 'Starbucks',
-    image: '../assets/team3.jpg',
+    name: 'Yavuz Selim Çakmak',
+    role: 'Full Stack Developer',
+    image: team33,
   },
   {
     name: 'Floyd Miles',
     role: 'Facebook',
-    image: '../assets/team4.jpg',
+    image: team4,
   },
   {
     name: 'Jane Cooper',
     role: 'Mitsubishi',
-    image: '../assets/team5.jpg',
+    image: team5,
   },
   {
     name: 'Robert Fox',
     role: 'IBM',
-    image: '../assets/team6.jpg',
+    image: team6,
   },
   {
     name: 'Leslie Alexander',
     role: 'The Walt Disney Company',
-    image: '../assets/team7.jpg',
+    image: team7,
   },
   {
     name: 'Jacob Jones',
     role: 'Starbucks',
-    image: '../assets/team8.jpg',
+    image: team8,
   },
 ];
 
